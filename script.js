6cb14182b293deaf73c9cb7142ecebc91b060a9d
Eduardo Pacheco <pacheco.eaguiar@gmail.com>
2022-08-13 01:16:52 -0300

    feat: first dump with first words

---
+++ script.js
@@ -0,0 +1,65 @@
+// State control constants
+const GREEK = 0
+const PORTUGUESE = 1
+
+// State control variables
+let index = 0
+
+// Define container
+const portugueseBox = document.querySelector('.portuguese-box')
+const portugueseWord = document.querySelector(".portuguese-word")
+const greekBox = document.querySelector(".greek-box")
+const greekWord = document.querySelector(".greek-word")
+const greekSub1 = document.querySelector(".greek-sub1")
+const greekSub2 = document.querySelector(".greek-sub2")
+const flipCardInner = document.querySelector(".flip-card-inner")
+const previousButton = document.querySelector(".caret-btn-prev")
+const nextButton = document.querySelector(".caret-btn-next")
+
+// Words control
+previousButton.onclick = () => setTimeout(() => {
+    index = index == 0 ? words.length - 1 : index - 1
+    writeWords()
+},
+    // Wait for translation to hide back
+    portugueseBox.style.display == "flex" ? 600 : 0)
+
+// Words control
+nextButton.onclick = () => setTimeout(() => {
+    index = index == words.length - 1 ? 0 : index + 1
+    writeWords()
+},
+    // Wait for translation to hide back
+    portugueseBox.style.display == "flex" ? 600 : 0)
+
+// Switch language after each transition
+flipCardInner.addEventListener("transitionstart", () => setTimeout(() => {
+    greekBox.style.display = greekBox.style.display == "flex" ? "none" : "flex"
+    portugueseBox.style.display = portugueseBox.style.display == "flex" ? "none" : "flex"
+}, 100))
+
+/**
+ * SEE: Constants and initial state variables were defined in init_vars.js
+ */
+
+init()
+
+// Define initial state
+function init() {
+    words = shuffle(words)
+    writeWords()
+}
+
+// Hide / Show pages given the current state index
+function writeWords() {
+    // Write dynamic data
+    portugueseWord.textContent = words[index].portuguese
+    greekWord.textContent = words[index].greek
+    greekSub1.textContent = words[index].sub1
+    greekSub2.textContent = words[index].sub2
+}
+
+// Shuffle array
+function shuffle(arr) {
+    return arr.sort(() => Math.random() - 0.5)
+}
---
+++ words.js
@@ -0,0 +1,82 @@
+const Noun = "noun"
+const Adjective = "adjective"
+
+let words = [
+  {
+    greek: "ἀδελφή",
+    sub1: "-ῆ",
+    sub2: "ἡ",
+    portuguese: "irmã",
+    clazz: Noun
+  },
+  {
+    greek: "ἀνήρ",
+    sub1: "ἀνδρός",
+    sub2: "ὁ",
+    portuguese: "homem; marido",
+    clazz: Noun
+  },
+  {
+    greek: "βασιλεία",
+    sub1: "-ɑς",
+    sub2: "ἡ",
+    portuguese: "reino",
+    clazz: Noun
+  },
+  {
+    greek: "γυνή",
+    sub1: "γυναῖκος",
+    sub2: "ἡ",
+    portuguese: "mulher; esposa",
+    clazz: Noun
+  },
+  {
+    greek: "δόξα",
+    sub1: "-ῆ",
+    sub2: "ἡ",
+    portuguese: "glória",
+    clazz: Noun
+  },
+  {
+    greek: "ζωή",
+    sub1: "-ῆ",
+    sub2: "ἡ",
+    portuguese: "vida (espiritual)",
+    clazz: Noun
+  },
+  {
+    greek: "καθαρός",
+    sub1: "-ά",
+    sub2: "ὁν",
+    portuguese: "puro(a)",
+    clazz: Adjective
+  },
+  {
+    greek: "κύριος",
+    sub1: "-ου",
+    sub2: "ὁ",
+    portuguese: "senhor",
+    clazz: Noun
+  },
+  {
+    greek: "λόγος",
+    sub1: "-ου",
+    sub2: "ὁ",
+    portuguese: "palavra",
+    clazz: Noun
+  },
+  {
+    greek: "μαθητής",
+    sub1: "οῦ",
+    sub2: "ὁ",
+    portuguese: "discípulo",
+    clazz: Noun
+  },
+  {
+    greek: "περιτομή",
+    sub1: "-ῆς",
+    sub2: "ή",
+    portuguese: "circunsição",
+    clazz: Noun
+  },
+];
